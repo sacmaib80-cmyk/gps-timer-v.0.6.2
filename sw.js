44e25f2fb9d463875387d61e4ed1464d8dee0030
git <sacmaib80@gmail.com>
2026-03-15 16:43:04 +0700

    test 0.8

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then((clientList) => {
+      for (const client of clientList) {
+        if ("focus" in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow("./");
+      }
+    })
+  );
+});
